65a8639c07e12c449f7a4e44f62269f03e73b382
Chry123 <christianshoraj@gmail.com>
2021-04-30 11:16:55 +0200

    # Esercitazione Canile
    Descrizione progetto:
    Il progetto prevede la realizzazione di una single page application che implementi un CRUD per la gestione dei canie e la conservazione degli stessi in un database.
    
    Struttura progetto:
    
    1)DATA
    
    La cartella DATA, contiene le classi e DataBase(le strutture dati), riguardanti i Cani:
    
    Cane.cs rappresenta un insieme di dati che riguardano i dati specifici di un Cane;
    BancaDati.cs rappresenta il DbContext e quindi la connessione al database;
    
    2)Pages
    
    Il contenuto di Pages descrive gli end point del progetto.
    Il quale nel mio progetto viene sopranominato Cani.razor.
    
    3) DataBase
    
    Canile.db rappresenta il database dei cani, in cui si andrà a a Salvare i dati oppure Creare nuovi campi in caso dovesse essere aggiunto un nuovo cane,
    Modificare se ci dovessere essere delle mofifiche da svolgere e infine Eliminare il rispettivo campo.
    
    ! [User flow]
    (C:\Users\Christian\Desktop\canile1.png)
    (C:\Users\Christian\Desktop\canile2.png)

---
+++ canile/Data/BancaDati.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace canile.Data
+{
+	public class BancaDati : DbContext
+	{
+		public DbSet<Cane> cani { get; set; }
+
+
+		public BancaDati(DbContextOptions options) : base(options) { }
+	}
+}
---
+++ canile/Data/Cane.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace canile.Data
+{
+    public class Cane
+    {
+
+       // MEGLIO NON FARE I CONSTRAICT
+
+        
+        /// <summary>
+        /// l'ID del cane
+        /// </summary>
+        [Required(ErrorMessage = " Inserire l' ID" )]
+        public int ID { get; set; }
+
+        /// <summary>
+        /// Convalida del nome
+        /// </summary>
+        [Required(ErrorMessage = " Inserire il nome")]
+        [StringLength(10, MinimumLength =1, ErrorMessage ="Il nome del cane deve avere una lunghezza minima di 1 e un massmo di 10!" )]
+        public string Nome { get; set; }
+
+        /// <summary>
+        /// Convalida per l' eta del cane
+        /// </summary>
+        [Required(ErrorMessage = " Inserire l' eta")]
+        public byte Eta { get; set; }
+
+        /// <summary>
+        /// Convalida per l' adozione del cane
+        /// </summary>
+        [Required(ErrorMessage = " Inserire la adozione")]
+        public DateTime Adozione { get; set; }
+
+        /// <summary>
+        /// convalida per la razza
+        /// </summary>
+        [Required(ErrorMessage = " Inserire la razza")]
+        [StringLength(20, MinimumLength = 1, ErrorMessage = "Il nome del cane deve avere una lunghezza minima di 1 e un massmo di 20!")]
+        public string Razza { get; set; }
+
+        /// <summary>
+        /// convalida per il genere
+        /// </summary>
+        [Required(ErrorMessage = " Inserire il genere")]
+        public bool Maschio { get; set; }
+
+        /// <summary>
+        /// convalida per l'aquisizione
+        /// </summary>
+        [Required(ErrorMessage = " Inserire la aquisizione")]
+        public DateTime Aquisizione { get; set; }
+
+        /// <summary>
+        /// convalida per la sterilizzazione
+        /// </summary>
+        [Required(ErrorMessage = " Inserire la sterilizazzione")]
+        public bool Sterilizzazione { get; set; }
+        
+    }
+}
